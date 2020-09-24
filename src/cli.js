c7cb28aef72ec6293a1434cc3a7579b99629a8fa
gustavo.farias <gustavogfarias142@gmail.com>
2020-09-24 18:22:22 -0300

    ajustes na criação de arquivos com código

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,10 @@ import inquirer from 'inquirer';
 import fs from 'fs';
 import readline from 'readline';
 import figlet from 'figlet';
+import base from 'nodejs-base64'
+const service  = 'aW1wb3J0IHsgaHR0cFVybCB9IGZyb20gJy4uL3BsdWdpbnMvaHR0cCc7DQoNCmV4cG9ydCBkZWZhdWx0IHsNCiAgICB0ZXN0RW5kcG9pbnQ6ICgpID0+IHsNCiAgICAgICAgcmV0dXJuIGh0dHBVcmwuZ2V0KCd1cmwnKTsNCiAgICB9DQp9'
+const styles = 'aW1wb3J0IHN0eWxlZCBmcm9tICdzdHlsZWQtY29tcG9uZW50cycNCg0KZXhwb3J0IGNvbnN0IENvbnRhaW5lciA9IHN0eWxlZC5kaXZgDQogICAgI2JvdGFvew0KICAgICAgbWFyZ2luOjEwcHggOyAgDQogICAgICBiYWNrZ3JvdW5kLWNvbG9yOndoaXRlOw0KICAgICAgY29sb3I6YmxhY2s7DQogICAgICBmb250LXdlaWdodDpib2xkOw0KICAgICAgd2lkdGg6MjAlOw0KICAgICAgDQogICAgfQ0KDQpg'
+const view = 'aW1wb3J0IFJlYWN0IGZyb20gJ3JlYWN0JzsNCmltcG9ydCB7IEJveCxCdXR0b24gfSBmcm9tICdAbWF0ZXJpYWwtdWkvY29yZScNCmltcG9ydCB7IENvbnRhaW5lciB9IGZyb20gJy4vc3R5bGUnOw0KDQpjbGFzcyBQYWdlIGV4dGVuZHMgUmVhY3QuQ29tcG9uZW50ew0KICAgIGV4YW1wbGVNZXRob2QoKXsNCiAgICAgICAgICAgIA0KICAgICAgICAgICAgDQogICAgICB9DQoNCiAgICByZW5kZXIoKXsNCiAgICAgICAgcmV0dXJuICgNCiAgICAgICAgPD4NCiAgICAgICAgPENvbnRhaW5lcj4NCiAgICAgICAgICAgIDxCb3ggb3ZlcmZsb3c9ImF1dG8iIHdpZHRoPSIxIiBoZWlnaHQ9IjEiPg0KICAgICAgICAgICAgICAgICAgICAgICAgICAgIDxCb3ggaWQ9InRpdGxlIj4NCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgPGRpdj4NCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICA8QnV0dG9uIGhyZWY9IiMiIG9uQ2xpY2s9e3RoaXMuZXhhbXBsZU1ldGhvZH0gaWQ9ImJvdGFvIiBzaXplPSJsYXJnZSIgY29sb3I9InByaW1hcnkiPlRlc3RlPC9CdXR0b24+DQogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIDwvZGl2PiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICANCiAgICAgICAgICAgICAgICAgICAgICAgICAgICA8L0JveD4NCiAgICAgICAgICAgIDwvQm94Pg0KICAgICAgICA8L0NvbnRhaW5lcj4NCg0KDQogICAgICAgIDwvPg0KICAgICAgICApOw0KICAgIH0NCn0NCg0KZXhwb3J0IGRlZmF1bHQgUGFnZTsNCg=='
 
 function parseArgumentsIntoOptions(rawArgs) {
     const args = arg(
@@ -62,17 +66,21 @@ function parseArgumentsIntoOptions(rawArgs) {
             input: process.stdin,
             output: process.stdout
         });
+        if(!fs.existsSync('./src/')){
+            fs.mkdirSync('./src/');
+        }
         
         rl.question('Digite o nome do arquivo: ', (answer) => {
             try{
                 if(options.file == 'View'){
+                    
                     fs.mkdirSync('./src/views');
                     fs.mkdirSync('./src/views/'+answer);
-                    fs.appendFile('./src/views/'+answer+'/index.tsx', fs.readFileSync('./src/templates/view.txt').toString(), function (err) {
+                    fs.appendFile('./src/views/'+answer+'/index.tsx',base.base64decode(view), function (err) {
                         if (err) throw err;
-                        console.log(options.file+' criado com sucesso!');
+                        console.log('\x1b[32m',options.file+' criado com sucesso!');
                       });
-                    fs.appendFile('./src/views/'+answer+'/style.js', fs.readFileSync('./src/templates/styles.txt').toString(), function (err) {
+                    fs.appendFile('./src/views/'+answer+'/style.js', base.base64decode(styles), function (err) {
                         if (err) throw err;
                         
                       });             
@@ -80,33 +88,35 @@ function parseArgumentsIntoOptions(rawArgs) {
                 }
                 if(options.file == 'Service'){
                     fs.mkdirSync('./src/services');
-                    fs.appendFile('./src/services/'+answer+'.js', fs.readFileSync('./src/templates/service.txt').toString(), function (err) {
+                    fs.appendFile('./src/services/'+answer+'.js', base.base64decode(service), function (err) {
                         if (err) throw err;
-                        console.log(options.file+' criado com sucesso!');
+                        console.log('\x1b[32m',options.file+' criado com sucesso!');
                       });
                 }
             }catch(e){
                 if(options.file == 'View'){
                     try{
                         fs.mkdirSync('./src/views/'+answer);
-                        fs.appendFile('./src/views/'+answer+'/index.tsx', fs.readFileSync('./src/templates/view.txt').toString(), function (err) {
+                        fs.appendFile('./src/views/'+answer+'/index.tsx', base.base64decode(view), function (err) {
                             if (err) throw err;
-                            console.log(options.file+' criado com sucesso!');
+                            console.log('\x1b[32m',options.file+' criado com sucesso!');
                         });
-                        fs.appendFile('./src/views/'+answer+'/style.js', fs.readFileSync('./src/templates/styles.txt').toString(), function (err) {
+                        fs.appendFile('./src/views/'+answer+'/style.js', base.base64decode(styles), function (err) {
                             if (err) throw err;
                             
                         });
                     }catch(ee){
-                        console.log('Erro ao criar arquivo, o mesmo já se encontra existente')                        
+                        
+                        console.log('\x1b[31m', 'Erro ao criar arquivo, o mesmo já se encontra existente');
+                                 
                     }
 
                 }
                 if(options.file == 'Service'){
                     
-                            fs.appendFile('./src/services/'+answer+'.js',  fs.readFileSync('./src/templates/service.txt').toString(), function (err) {
+                            fs.appendFile('./src/services/'+answer+'.js',  base.base64decode(service), function (err) {
                             if (err) throw err;
-                            console.log(options.file+' criado com sucesso!');
+                            console.log('\x1b[32m',options.file+' criado com sucesso!');
                       });
                      
                 }
@@ -126,12 +136,14 @@ function parseArgumentsIntoOptions(rawArgs) {
             return;
         }
         console.log(data)
-        console.log('LISTA DE COMANDOS')
-        console.log('(--create ou -C) ----------------- Criar arquivos no projeto (Views, Services)');
+        console.log('\x1b[32m','LISTA DE COMANDOS')
+        console.log('\x1b[32m','(--create ou -C) ----------------- Criar arquivos no projeto (Views, Services)');
     });
    }
    
    export async function cli(args) {
+    
+   
     try{
         let options = parseArgumentsIntoOptions(args);
         options = await promptForMissingOptions(options);
